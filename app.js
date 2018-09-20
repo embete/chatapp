42999cc71a645273955fcf8eec173f75feb0844e
Christian Rennemo <christian.rennemo@gmail.com>
2018-09-20 22:03:47 +0200

    Handling number of users, and deletes all data when all users has left the chat

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
+// URL's 
+const urlMessages = 'https://chatapp-217010.appspot.com/messages'
+// const urlMessages = 'http://127.0.0.1:3001/messages'
+
 var socket = io()
 const divMessages= document.querySelector('#messages')
+const text = document.querySelector('#text')
+const activeUsers = document.querySelector('#total')
 
 // OnLoad
 const load = () => {
-  console.log("load event detected!")
   // Take name and place in top
   let givename = prompt('Whats your chat name?')
+  if (givename===null || givename==='')
+    givename = 'anonymous'
+    
   document.querySelector('#name').innerHTML = givename
   getMessages()
-
-  // OnClick send
-  document.querySelector('#send').addEventListener('click', function() {
-    var text = document.querySelector('#text')
-    var name = document.querySelector('#name')
-    var message = { name: name.innerHTML, message: text.value };
-    postMessage(message)
-    text.value = ''
-    text.focus()
-  })
+  document.querySelector('#send').addEventListener('click', formSubmit)  
 }
 
-socket.on('message', addMessage)
+socket.on('message', addMessage) // Display new message
+socket.on('userUpdate', displayActiveUsers) // when new user arrive
+
+// Socket for new user connecting
+socket.on('connect', function () { // New user, tell server
+  socket.emit('newuser', function (data) {
+    console.log(data); // data will be number of users
+    displayActiveUsers(data)
+  });
+});
+
 window.onload = load;
 
+function formSubmit() {
+  var name = document.querySelector('#name')
+  var message = { name: name.innerHTML, message: text.value };
+  postMessage(message)
+  text.value = ''
+  text.focus()
+}
+
+// Display number of people in chat
+function displayActiveUsers(number) {
+  activeUsers.innerHTML = number
+}
+
 function getMessages() {
-  fetch('https://chatapp-217010.appspot.com/messages')
+  fetch(urlMessages)
     .then(function (response) {
       return response.json();
     })
     .then(function (myJson) {
       console.log(JSON.stringify(myJson))      
       myJson.forEach(element => {
-        addMessage(element) // Lynda sender denne uten element som parameter, kun tom. Hvorfor funker det?
+        addMessage(element)
       });
     });
 }
 
 function postMessage(message) {
-  fetch('https://chatapp-217010.appspot.com/messages', {
+  fetch(urlMessages, {
     method: 'POST',
     body: JSON.stringify(message),
     headers:{
@@ -50,5 +72,14 @@ function postMessage(message) {
 }
 
 function addMessage(message) {
-  divMessages.innerHTML += `<div class="name"> ${message.name} </div><div class="text"> ${message.message }</div>`
+  divMessages.innerHTML += `<div class="wrapper"><div class="name"> ${message.name} </div><div class="text"> ${message.message }</div></div>`
 }
+
+// Prevent input enter send
+text.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    formSubmit()
+  }
+});
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Local vs online
+var port = 8080 // online
+// var port = 3001 // local
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
@@ -10,19 +14,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 var dburl = 'mongodb://user:User312@ds163182.mlab.com:63182/chatapp'
+var participants = 0
 
+// DB Model
 var Message = mongoose.model('Message', {
   name: String,
   message: String
 })
 
-
+// Get messages
 app.get('/messages', (req, res) => {
   Message.find({}, (err, messages) => {
     res.send(messages) 
   })
 })
 
+// Post message
 app.post('/messages', (req, res) => {
   var message = new Message(req.body)
 
@@ -33,18 +40,41 @@ app.post('/messages', (req, res) => {
     io.emit('message', req.body)
     res.sendStatus(200)
   })
-  
 })
 
+// Dedect user
 io.on('connection', (socket) => {
+  participants++;
+  socket.broadcast.emit('userUpdate', participants); // Tell all user the new number of total
+  socket.on('newuser', function (number) { // Tell only the new user total (can't get on same emit as everone else)
+    number(participants);
+  });
   console.log('a user is connected');
+  console.log('number of acitve users: '+ participants)
+  socket.on('disconnect', function () { 
+    socket.broadcast.emit('userUpdate', participants);
+    console.log('a user has disconneected');
+    participants--
+    console.log('number of acitve users: '+ participants)
+    if (participants<=0) {
+      console.log('no more active people, delete all data');
+      deleteAllData()
+    }
+  })
 })
 
+function deleteAllData() {
+  Message.remove({}, (err) => {
+    console.log('all messages and usernames deleted', err);
+  })
+}
+
+// Conect to DB (mLab)
 mongoose.connect(dburl, {useNewUrlParser: true}, (err) => {
   console.log('mongo db connection', err)
-  
 })
 
-var server = http.listen(8080, () => {
+// Port listening at server
+var server = http.listen(port, () => {
   console.log('Server is listiening on port', server.address().port);
 })
